b67df27c3770cd9f8efdd2eae05429173b61106f
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2025-08-09 21:56:24 +0600

    responsib add

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-let navtoggler = document.querySelector("#nav-toggler");
-let top_last_navigation = document.querySelector(".top_last_navigation");
-navtoggler.addEventListener("click",()=>{
-    top_last_navigation.classList.toggle("open-mobilesidenav");
+// let navtoggler = document.querySelector("#nav-toggler");
+// let top_last_navigation = document.querySelector(".top_last_navigation");
+// navtoggler.addEventListener("click",()=>{
+//     top_last_navigation.classList.toggle("open-mobilesidenav");
+// })
+// document.querySelector(".m_close_btn").addEventListener("click",()=>{
+//     top_last_navigation.classList.remove("open-mobilesidenav");
+// })
+document.body.addEventListener("click",(event)=>{
+    const targetId = event.target.getAttribute("data-bs-toggler-custom");
+    if(targetId){
+        const targetElement = document.getElementById(targetId);
+        if(targetElement){
+            targetElement.classList.toggle("open-mobilesidenav");
+        } 
+    }
 })
-document.querySelector(".m_close_btn").addEventListener("click",()=>{
-    top_last_navigation.classList.remove("open-mobilesidenav");
+document.body.addEventListener("click",(e)=>{
+    const targetId_close = e.target.getAttribute("data-toggle-closeer");
+    if(targetId_close){
+        const targetElement_close = document.getElementById(targetId_close);
+        if(targetElement_close){
+            targetElement_close.classList.remove("open-mobilesidenav")
+        }
+    }
 })
