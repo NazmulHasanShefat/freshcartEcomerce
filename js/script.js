60482e5c763900fcd24b42b4794a958fefc14ed0
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2025-08-11 18:06:42 +0600

    last commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
-// let navtoggler = document.querySelector("#nav-toggler");
-// let top_last_navigation = document.querySelector(".top_last_navigation");
-// navtoggler.addEventListener("click",()=>{
-//     top_last_navigation.classList.toggle("open-mobilesidenav");
-// })
-// document.querySelector(".m_close_btn").addEventListener("click",()=>{
-//     top_last_navigation.classList.remove("open-mobilesidenav");
-// })
 document.body.addEventListener("click", (event) => {
     const targetId = event.target.getAttribute("data-bs-toggler-custom");
     const targetId2 = event.target.getAttribute("data-target-sub-item");
-    
-    if(targetId2){
-        const targetElement2 = document.getElementById(targetId2);
-        if(targetElement2){
-            // console.log(targetElement2.scrollHeight);
-        }
-    }
 
     if (targetId) {
         const targetElement = document.getElementById(targetId);
         if (targetElement) {
             targetElement.classList.toggle("open-mobilesidenav");
+        }
+    }
+
+     if (targetId2) {
+        const targetElement2 = document.getElementById(targetId2);
+        if (targetElement2) {
+            targetElement2.classList.toggle("activeHeight");
+            if(targetElement2.classList.contains("activeHeight")){
+                 targetElement2.style.maxHeight = targetElement2.scrollHeight + "px";
+            }else{
+                targetElement2.style.maxHeight = "0";
+            }
         }
     }
 })
@@ -35,18 +32,3 @@ document.body.addEventListener("click", (e) => {
         }
     }
 })
-
-// with animation 
-// let myli = document.querySelectorAll("ul");
-
-// myli.forEach(allList => {
-//     allList.addEventListener("click", (e) => {
-//         const targetId = e.target.getAttribute("data-target-sub-item");
-//         if (targetId) {
-//             const targetElement = document.getElementById(targetId);
-//             if (targetElement) {
-//                 console.log(targetElement.scrollHeight);
-//             }
-//         }
-//     })
-// });
