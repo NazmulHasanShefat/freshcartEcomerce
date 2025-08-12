14d3d73c038709ae8ba0a9a8a5796f6402ece9f1
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2025-08-12 22:40:46 +0600

    last commit

--- js/count_down.js
+++ js/count_down.js
@@ -1,27 +1,40 @@
 let set_btn = document.querySelector("#set_btn");
 let offerTime = document.querySelector("#offerTime");
+let dayNumber = document.querySelector(".dayNumber");
+let H_Number = document.querySelector(".H_Number");
+let Min_Number = document.querySelector(".M_Number");
+let Sec_Number = document.querySelector(".S_Number");
 
-offerTime.addEventListener("input",()=>{
+function updateDateTime(){
     // let cd_value = current_date.value ;
     let c_offer_value = offerTime.value ;
-    if(c_offer_value){
-        let offerDate = c_offer_value;
-        let t = new Date();
-        let dd = t.getDate();
-        let mm = t.getMonth() + 1;
-        let yy = t.getFullYear();
-        let chusen = new Date(offerDate);
-        let totaldeff = chusen - t;
-        // we comverted milisec to sec
-        let sec = Math.floor(totaldeff / 1000);
-        // convert sec to minit 60sec = 1min becous we
-        // devite 60 with total min
-        let min = Math.floor(sec / 60);
-        let hour = Math.floor(min / 60);
-        // 24 hour = 1 day
-        let day = Math.floor(hour / 24)
-        console.log(day);
-    }else{
-        alert("please enter date")
-    }
-})
+    let offerDate = c_offer_value;
+    let t = new Date();
+    let myd = new Date(`${c_offer_value}`)
+    let dd = t.getDate();
+    let mm = t.getMonth() + 1;
+    let yy = t.getFullYear();
+    let chusen = new Date(offerDate);
+    let totaldeff = myd - t;
+    // we comverted milisec to sec
+    let sec = Math.floor((totaldeff / 1000)); //total secend
+    // convert sec to minit 60sec = 1min becous we
+    // devite 60 with total min
+    let min = Math.floor(sec / 60);
+    let hour = Math.floor(min / 60);
+    // 24 hour = 1 day
+    let day = Math.floor(hour / 24)
+    let hours = hour % 24;
+    let minit = min % 60;
+    let secend = sec % 60;
+    // console.log(`toal days:${day} | total hours: ${hour} | total min: ${min} | total sec:${sec}`);
+    // console.log(chusen)
+    dayNumber.innerText = day;
+    H_Number.innerText = String(hours).padStart(2,'0');
+    Min_Number.innerText = String(minit).padStart(2,'0');
+    Sec_Number.innerText = String(secend).padStart(2,'0');
+}
+
+offerTime.addEventListener("input",updateDateTime)
+
+setInterval(updateDateTime ,1000)
---
+++ test.js
@@ -0,0 +1,3 @@
+
+console.log(String(523423).padStart(2, '0'))   // "05"
+console.log(String(12).padStart(2, '0'))  // "12"
